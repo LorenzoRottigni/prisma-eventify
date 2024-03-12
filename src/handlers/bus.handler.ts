b258aa9b2adb2b05c58ac27a99ffd6f40fe5684d
LorenzoRottigni <lorenzo@rottigni.net>
2024-03-12 12:45:22 +0100

    feat: :sparkles: busHandler.subscribeConfigEvents, busHandler.publishEvent

--- src/handlers/bus.handler.ts
+++ src/handlers/bus.handler.ts
@@ -1,5 +1,26 @@
 import { EventBus } from 'ts-bus'
+import * as events from './../../codegen/events'
+import config from './../../config.events'
 
 export class BusHandler {
   constructor(public bus = new EventBus()) {}
+
+  public subscribeConfigEvents() {
+    Object.entries(config).forEach(([event, callback]) => {
+      this.bus.subscribe(events[event], callback)
+    })
+  }
+
+  /**
+   * @description Generated service side:
+   * this.busHandler.publishEvent('UserBeforeFindMany', { args, prisma: this.prisma })
+   * @param event
+   * @param meta
+   * @returns
+   */
+  public publishEvent(event: string, meta: any) {
+    if (!events?.[event]) return false
+    this.bus.publish(events[event], meta)
+    return true
+  }
 }
