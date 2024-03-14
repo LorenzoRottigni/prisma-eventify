015838fdf650171d38e21c50a1f798773a666638
LorenzoRottigni <lorenzo@rottigni.net>
2024-03-14 11:11:58 +0100

    refactor: :art: refactor tests directory

--- tests/data/eventify.config.ts
+++
@@ -1,9 +0,0 @@
-import { EventifyConfig } from '../../src/types/config'
-
-const config: EventifyConfig = {
-  excludeFields: ['id'],
-  excludeModels: [],
-  outDir: './dist/bundle',
-}
-
-export default config
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,11 +1,18 @@
 import { getDMMF } from '@prisma/internals'
-import { ecommerceDatamodel as datamodel } from './data/ecommerce.schema'
+import { ecommerceDatamodel as datamodel } from './prisma/schema.prisma'
 import ServiceGenerator from '../src/generators/service.generator'
-import config from './data/eventify.config'
-import fs from 'fs'
 import { PrismaService } from '../src/services/prisma.service'
 import { ConfigService } from '../src/services/config.service'
 import { EventGenerator } from '../src/generators/eventify.generator'
+import { EventifyConfig } from '../src/types/config'
+import { BusHandler } from './../src/handlers/bus.handler'
+import { UserService } from './../dist/bundle/services/user.service'
+
+const config: EventifyConfig = {
+  excludeFields: ['id'],
+  excludeModels: [],
+  outDir: './dist/bundle',
+}
 
 describe('Model Services Generator', () => {
   it('Should generate services bundle.', async () => {
@@ -24,21 +31,21 @@ describe('Model Services Generator', () => {
     expect(generator.generateBundle()).toBe(true)
   })
 
-  // it('Should create a new user.', async () => {
-  //   const schema = await getDMMF({ datamodel })
-  //   const prismaService = new PrismaService(schema)
-  //   const configService = new ConfigService(config)
-  //   const busHandler = new BusHandler(prismaService, configService)
-  //   const userService = new UserService(busHandler)
-  //   const user = await userService.create({
-  //     data: {
-  //       email: 'lorenzo@rottigni.tech',
-  //       password: 'password',
-  //       username: 'lorenzorottigni',
-  //       createdAt: new Date(),
-  //     },
-  //   })
-  //
-  //   expect(user).toBeTruthy()
-  // })
+  it('Should create a new user.', async () => {
+    const schema = await getDMMF({ datamodel })
+    const prismaService = new PrismaService(schema)
+    const configService = new ConfigService(config)
+    const busHandler = new BusHandler(prismaService, configService)
+    const userService = new UserService(busHandler)
+    const user = await userService.create({
+      data: {
+        email: 'lorenzo@rottigni.tech',
+        password: 'password',
+        username: 'lorenzorottigni',
+        createdAt: new Date(),
+      },
+    })
+
+    expect(user).toBeTruthy()
+  })
 })
